f135152a8feb1b91b0fbb9f17a4a528f46e0b9c3
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-01 15:37:56 -0800

    Camera update
    pic kinda works now! I did it in a probably dumb way, but it works and thats alll that matters lol

--- player/playerhead.gd
+++ player/playerhead.gd
@@ -4,6 +4,8 @@ extends Node3D
 @onready var cam_vp:SubViewport          = get_node("player_cam/SubViewport")
 @onready var cam_flash:SpotLight3D     = get_node("player_cam/CameraFLash")
 @onready var cam_screen:MeshInstance3D = get_node("player_cam/MeshInstance3D")
+@onready var cam_sprite:Sprite3D = get_node("player_cam/Sprite3D")
+@onready var cam_sprite_Screen:Sprite3D = get_node("player_cam/spriteScreen")
 @onready var cam_follow_movement:Node3D = get_node("player_cam/SubViewport/ROOT")
 
 # Called when the node enters the scene tree for the first time.
@@ -26,22 +28,29 @@ func take_pic():
 	await get_tree().create_timer(.2).timeout
 	cam_flash.light_energy = 4.0
 	
+	#wait for flash and take pic, save pic
 	await get_tree().process_frame
 	await get_tree().process_frame
 	await get_tree().process_frame
-
-	var image = cam_vp.get_texture().get_image()
-	image.flip_y()
-	image.save_png("user://imagetest.png") #C:\Users\jared\AppData\Roaming\Godot\app_userdata\Acerola Game Jam
-	var imagetex = ImageTexture.create_from_image(image)
+	var image:Image = cam_sprite.get_texture().get_image()
+	image.flip_x()
+	save_img(image)
 	
 	await get_tree().create_timer(.05).timeout
 	cam_flash.light_energy = 0
 	
-	var og_material = cam_screen.mesh.surface_get_material(0)
-	cam_screen.mesh.surface_set_material(0, imagetex)
+	var imagetex = ImageTexture.create_from_image(image)
+	set_cam_screen(imagetex)
+
 	
+func save_img(cam_img:Image):
+	cam_img.save_png("user://imagetest.png") #C:\Users\jared\AppData\Roaming\Godot\app_userdata\Acerola Game Jam
+	
+func set_cam_screen(cam_img_tex:ImageTexture):
+	#var og_material = cam_screen.mesh.surface_get_material(0)
+	#cam_screen.mesh.surface_set_material(0, cam_img_tex)
+	var og_material = cam_sprite_Screen.get_texture()
+	cam_sprite_Screen.set_texture(cam_img_tex)
 	await get_tree().create_timer(3).timeout
 	
-	cam_screen.mesh.surface_set_material(0, og_material)
-	
+	cam_sprite_Screen.set_texture(ImageTexture.create_from_image(load("res://player/black.png")))
