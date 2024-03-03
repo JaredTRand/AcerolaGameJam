a9e9bebc799a87e81b680caeda0f13d0d0d89127
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-02 23:03:01 -0800

    new camera update
    can zoom in on the image now. added better timers for the images appearing on the cam

---
+++ player/cam/handheld_cam.gd
@@ -0,0 +1,83 @@
+extends Node3D
+
+@onready var lens = get_node("camera_lens_loc")
+@onready var cam_vp:SubViewport          = get_node("SubViewport")
+@onready var cam_flash:SpotLight3D     = get_node("CameraFLash")
+@onready var cam_sprite:Sprite3D = get_node("Sprite3D")
+@onready var cam_sprite_Screen:Sprite3D = get_node("spriteScreen")
+@onready var cam_follow_movement:Node3D = get_node("SubViewport/ROOT")
+@onready var cam_sound_player:AudioStreamPlayer3D = get_node("AudioStreamPlayer3D")
+
+@onready var cam_cooldown_timer:Timer = get_node("timers/cam_cooldown")
+@onready var cam_preview_image_cooldown:Timer = get_node("timers/cam_preview_image_cooldown")
+
+const ADS_LERP:int = 20
+@export var default_pos:Vector3
+@export var ads_pos:Vector3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	cam_follow_movement.global_transform = lens.global_transform
+	
+	#bring camera close
+	if Input.is_action_pressed("secondary_action"):
+		transform.origin = transform.origin.lerp(ads_pos, ADS_LERP*delta)
+		cam_preview_image_cooldown.start()
+	else:
+		transform.origin = transform.origin.lerp(default_pos, ADS_LERP*delta)
+		
+	
+	
+func _input(event):
+	if Input.is_action_just_pressed("main_action"):
+		if cam_cooldown_timer.is_stopped():
+			take_pic()
+	
+
+func take_pic():
+	cam_cooldown_timer.start()
+	cam_flash.light_energy = 4.0
+	await get_tree().create_timer(.3).timeout
+	cam_flash.light_energy = 0
+	await get_tree().create_timer(.2).timeout
+	cam_flash.light_energy = 4.0
+	
+	#wait for flash and take pic, save pic
+	await get_tree().process_frame
+	await get_tree().process_frame
+	await get_tree().process_frame
+	var image:Image = cam_sprite.get_texture().get_image()
+	image.flip_x()
+	save_img(image)
+	
+	if !cam_sound_player.is_playing(): 
+		 #just to give the sound a litte variety
+		cam_sound_player.set_max_db(RandomNumberGenerator.new().randf_range(-3.0, -1.5))
+		cam_sound_player.set_pitch_scale(RandomNumberGenerator.new().randf_range(.90, 1.1))
+		cam_sound_player.play()
+	
+	await get_tree().create_timer(.05).timeout
+	cam_flash.light_energy = 0
+	
+	var imagetex = ImageTexture.create_from_image(image)
+	set_cam_screen(imagetex)
+
+	
+func save_img(cam_img:Image):
+	cam_img.save_png("user://imagetest.png") #C:\Users\jared\AppData\Roaming\Godot\app_userdata\Acerola Game Jam
+	
+func set_cam_screen(cam_img_tex:ImageTexture):
+	var og_material = cam_sprite_Screen.get_texture()
+	
+	cam_sprite_Screen.set_texture(cam_img_tex)
+	cam_sprite_Screen.get_window()
+	cam_preview_image_cooldown.start()
+	
+
+func _on_cam_preview_image_cooldown_timeout():
+	cam_sprite_Screen.set_texture(ImageTexture.create_from_image(load("res://player/cam/black.png")))
--- player/playerhead.gd
+++ player/playerhead.gd
@@ -1,12 +1,5 @@
 extends Node3D
 
-#@onready var cam = get_node("player_cam/CameraFLash")
-@onready var cam_vp:SubViewport          = get_node("player_cam/SubViewport")
-@onready var cam_flash:SpotLight3D     = get_node("player_cam/CameraFLash")
-@onready var cam_screen:MeshInstance3D = get_node("player_cam/MeshInstance3D")
-@onready var cam_sprite:Sprite3D = get_node("player_cam/Sprite3D")
-@onready var cam_sprite_Screen:Sprite3D = get_node("player_cam/spriteScreen")
-@onready var cam_follow_movement:Node3D = get_node("player_cam/SubViewport/ROOT")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,42 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	cam_follow_movement.global_transform = global_transform
+	pass
 	
 func _input(event):
-	if Input.is_action_just_pressed("main_action"):
-		take_pic()
-
-func take_pic():
-	cam_flash.light_energy = 4.0
-	await get_tree().create_timer(.3).timeout
-	cam_flash.light_energy = 0
-	await get_tree().create_timer(.2).timeout
-	cam_flash.light_energy = 4.0
-	
-	#wait for flash and take pic, save pic
-	await get_tree().process_frame
-	await get_tree().process_frame
-	await get_tree().process_frame
-	var image:Image = cam_sprite.get_texture().get_image()
-	image.flip_x()
-	save_img(image)
-	
-	await get_tree().create_timer(.05).timeout
-	cam_flash.light_energy = 0
-	
-	var imagetex = ImageTexture.create_from_image(image)
-	set_cam_screen(imagetex)
-
-	
-func save_img(cam_img:Image):
-	cam_img.save_png("user://imagetest.png") #C:\Users\jared\AppData\Roaming\Godot\app_userdata\Acerola Game Jam
-	
-func set_cam_screen(cam_img_tex:ImageTexture):
-	#var og_material = cam_screen.mesh.surface_get_material(0)
-	#cam_screen.mesh.surface_set_material(0, cam_img_tex)
-	var og_material = cam_sprite_Screen.get_texture()
-	cam_sprite_Screen.set_texture(cam_img_tex)
-	await get_tree().create_timer(3).timeout
-	
-	cam_sprite_Screen.set_texture(ImageTexture.create_from_image(load("res://player/black.png")))
+	pass
