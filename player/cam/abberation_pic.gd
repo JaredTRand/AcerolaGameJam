3e09ef7d25fee9784508266e312394f7818c9eb1
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-03 02:17:08 -0800

    Cam update
    Cam can now view previous pics using Q and E. can fav images

---
+++ player/cam/abberation_pic.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name Aberration_Image
+
+@export var ab_image:Image
+@export var starred:bool
+
+var prev_img:Aberration_Image
+var next_img:Aberration_Image
--- player/cam/handheld_cam.gd
+++ player/cam/handheld_cam.gd
@@ -2,17 +2,26 @@ extends Node3D
 
 @onready var lens = get_node("camera_lens_loc")
 @onready var cam_flash:SpotLight3D     = get_node("CameraFLash")
-@onready var cam_sprite:Sprite3D = get_node("Sprite3D")
-@onready var cam_sprite_Screen:Sprite3D = get_node("spriteScreen")
 @onready var cam_follow_movement:Node3D = get_node("SubViewport/ROOT")
 @onready var cam_sound_player:AudioStreamPlayer3D = get_node("AudioStreamPlayer3D")
+
+@onready var cam_sprite:Sprite3D = get_node("sprites/Sprite3D")
+@onready var cam_sprite_Screen:Sprite3D = get_node("sprites/spriteScreen")
+@onready var starred:Sprite3D = get_node("sprites/starred")
 
 @onready var cam_cooldown_timer:Timer = get_node("timers/cam_cooldown")
 @onready var cam_preview_image_cooldown:Timer = get_node("timers/cam_preview_image_cooldown")
 
+@onready var black_screen:Image = load("res://player/cam/black.png").get_image()
+
+@export var flash_brightness:float
+
 const ADS_LERP:int = 20
 @export var default_pos:Vector3
 @export var ads_pos:Vector3
+
+var images_array:Array
+var current_image:Aberration_Image
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,7 +30,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	cam_follow_movement.global_transform = lens.global_transform
+	cam_follow_movement.global_transform = lens.global_transform # make handheld_viewport_cam follow with cam mesh
+		
+	# take a pic
+	if Input.is_action_pressed("main_action"):
+		if cam_cooldown_timer.is_stopped():
+			take_pic()
 	
 	#bring camera close
 	if Input.is_action_pressed("secondary_action"):
@@ -30,7 +44,35 @@ func _process(delta):
 			cam_sound_player.set_max_db(RandomNumberGenerator.new().randf_range(-15.0, -13.5))
 			cam_sound_player.set_pitch_scale(RandomNumberGenerator.new().randf_range(3, 2))
 			cam_sound_player.play()
+			
 		transform.origin = transform.origin.lerp(ads_pos, ADS_LERP*delta)
+		
+		if(current_image != null):
+			set_cam_screen_ab(current_image)
+		if(images_array.size() > 0):
+			if Input.is_action_just_pressed("cam_prev_image"):
+				if(current_image.prev_img != null):
+					current_image = current_image.prev_img
+					set_cam_screen_ab(current_image)
+			elif Input.is_action_just_pressed("cam_next_image"):
+				if(current_image.next_img != null):
+					current_image = current_image.next_img
+					set_cam_screen_ab(current_image)
+			elif Input.is_action_just_pressed("cam_delete_image"):
+				var temp_img:Aberration_Image
+				
+				if(current_image.next_img != null):
+					temp_img = current_image.next_img
+				elif(current_image.prev_img != null):
+					temp_img = current_image.next_img
+					
+				images_array.remove_at(images_array.find(current_image))
+				
+				if(temp_img != null):
+					set_cam_screen_ab(temp_img)
+				else:
+					set_cam_screen(black_screen)
+				
 		cam_preview_image_cooldown.start()
 	else:
 		if Input.is_action_just_released("secondary_action"):
@@ -40,29 +82,30 @@ func _process(delta):
 			cam_sound_player.play()
 		transform.origin = transform.origin.lerp(default_pos, ADS_LERP*delta)
 		
-	
-	
-func _input(event):
-	if Input.is_action_just_pressed("main_action"):
-		if cam_cooldown_timer.is_stopped():
-			take_pic()
-	
+	if Input.is_action_just_pressed("cam_favorite_img"):
+		if(current_image.starred):
+			current_image.starred = false
+			starred.visible = false
+		else: 
+			current_image.starred = true
+			starred.visible = true
 
 func take_pic():
 	cam_cooldown_timer.start()
-	cam_flash.light_energy = 4.0
+	cam_flash.light_energy = flash_brightness
 	await get_tree().create_timer(.3).timeout
 	cam_flash.light_energy = 0
 	await get_tree().create_timer(.2).timeout
-	cam_flash.light_energy = 4.0
+	cam_flash.light_energy = flash_brightness
 	
 	#wait for flash and take pic, save pic
 	await get_tree().process_frame
 	await get_tree().process_frame
 	await get_tree().process_frame
 	var image:Image = cam_sprite.get_texture().get_image()
-	image.flip_x()
+	#image.flip_x()
 	save_img(image)
+	cam_flash.light_energy = 0
 	
 	if !cam_sound_player.is_playing(): 
 		 #just to give the sound a litte variety
@@ -72,22 +115,39 @@ func take_pic():
 		cam_sound_player.play()
 	
 	await get_tree().create_timer(.05).timeout
-	cam_flash.light_energy = 0
-	
-	var imagetex = ImageTexture.create_from_image(image)
-	set_cam_screen(imagetex)
+	set_cam_screen(image)
 
 	
 func save_img(cam_img:Image):
-	cam_img.save_png("user://imagetest.png") #C:\Users\jared\AppData\Roaming\Godot\app_userdata\Acerola Game Jam
+	var new_ab_img:Aberration_Image = Aberration_Image.new()
+	new_ab_img.ab_image = cam_img
 	
-func set_cam_screen(cam_img_tex:ImageTexture):
+	if(images_array.size() > 0):
+		images_array[images_array.size()-1].next_img = new_ab_img
+		new_ab_img.prev_img = images_array[images_array.size()-1]
+	
+	current_image = new_ab_img
+	images_array.push_back(new_ab_img)
+	
+func set_cam_screen_ab(cam_img:Aberration_Image):
+	if(cam_img.starred):
+		starred.visible = true
+	else:
+		starred.visible = false
+	set_cam_screen(cam_img.ab_image)
+	
+func set_cam_screen(cam_img:Image):
+	var imagetex = ImageTexture.create_from_image(cam_img)
 	var og_material = cam_sprite_Screen.get_texture()
 	
-	cam_sprite_Screen.set_texture(cam_img_tex)
+	cam_sprite_Screen.set_texture(imagetex)
 	cam_sprite_Screen.get_window()
 	cam_preview_image_cooldown.start()
-	
 
 func _on_cam_preview_image_cooldown_timeout():
-	cam_sprite_Screen.set_texture(ImageTexture.create_from_image(load("res://player/cam/black.png")))
+	if(images_array.size() > 0):
+		current_image = images_array[images_array.size()-1]
+	set_cam_screen(black_screen)
+	starred.visible = false
+	#cam_sprite_Screen.set_texture(ImageTexture.create_from_image())
+
