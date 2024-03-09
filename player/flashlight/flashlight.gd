dec074d4ed06ca38f640e2730df3eae338ad1b92
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-08 22:29:37 -0800

    setting up first level

--- player/flashlight/flashlight.gd
+++ player/flashlight/flashlight.gd
@@ -1,10 +1,16 @@
 extends Node3D
 
 @export var default_pos:Vector3
+@export var default_rot:Quaternion
 @export var ads_pos:Vector3
+@export var ads_rot:Quaternion
+
 const ADS_LERP:int = 10
+var smoothrot
+@onready var current_rot = Quaternion(transform.basis)
 
 @onready var turn_flash_timer:Timer = $turn_on_flash
+var flashlight_active:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,9 +20,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("flashlight"):
-		print_debug("falshlight")
-		#if transform.origin == default_pos:
+		if flashlight_active:
+			flashlight_active = false
+			smoothrot = current_rot.slerp(ads_rot, ADS_LERP)
+		else:
+			flashlight_active = true
+			smoothrot = current_rot.slerp(default_rot, ADS_LERP)
+			
+	#transform.basis = Basis(smoothrot)
+	if flashlight_active and transform.origin != ads_pos:
 		transform.origin = transform.origin.lerp(ads_pos, ADS_LERP*delta)
-		
-		#elif transform.origin == ads_pos:
-			#transform.origin = transform.origin.lerp(default_pos, ADS_LERP*delta)
+	elif not flashlight_active and transform.origin != default_pos:
+		transform.origin = transform.origin.lerp(default_pos, ADS_LERP*delta)
