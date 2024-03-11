bbbe2683085e3ea78b43beecf2f28e37d741b544
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-11 09:38:24 -0700

    Update Aberrition.gd

--- Aberritions/Aberrition.gd
+++ Aberritions/Aberrition.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+var noise_fading_in = false 
+
 @export ab_noise
 
 func _ready():
@@ -7,7 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#is there a good way to determine if this can be skipped so it doesnt do this tween every frame? or is it necessary rn
+	if noise_fading_in:
+		pass #tween audio louder
+	else:
+		pass #tween audio quieter
 
-func get_noise():
-  return ab_noise
+func fade_up_noise():
+	noise_fading_in = true
+
+func fade_out_noise():
+	noise_fading_in = false
