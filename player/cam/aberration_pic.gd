a939dad5dd2bf63b837118b256d2ff803806414b
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-11 10:28:46 -0700

    Update aberration_pic.gd

--- player/cam/aberration_pic.gd
+++ player/cam/aberration_pic.gd
@@ -2,8 +2,8 @@ extends Resource
 class_name Aberration_Image
 
 @export var ab_image:Image
-var starred:bool
-var to_submit:bool
+var starred:bool # to prevent accidental deletion in game
+var to_submit:bool # to mark which ones to submit at end of level
 
 var prev_img:Aberration_Image
 var next_img:Aberration_Image
