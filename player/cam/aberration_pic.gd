56c68c5fde41093477f712e96f8e9568494045aa
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-11 10:28:16 -0700

    Update aberration_pic.gd

--- player/cam/aberration_pic.gd
+++ player/cam/aberration_pic.gd
@@ -2,7 +2,8 @@ extends Resource
 class_name Aberration_Image
 
 @export var ab_image:Image
-@export var starred:bool
+var starred:bool
+var to_submit:bool
 
 var prev_img:Aberration_Image
 var next_img:Aberration_Image
