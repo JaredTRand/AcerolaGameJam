b05e94f515f295e48a8cf2a06d37ab074d97e83f
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-11 09:27:33 -0700

    Create Aberrition.gd

---
+++ Aberritions/Aberrition.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@export ab_noise
+
+func _ready():
+  pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func get_noise():
+  return ab_noise
