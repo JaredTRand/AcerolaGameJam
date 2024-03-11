c3f6388860de694cd0d32176bc3b20e3736cb0a4
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-11 09:39:16 -0700

    Update aberration_spawn_area.gd

--- Aberritions/aberration_spawn_area.gd
+++ Aberritions/aberration_spawn_area.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 var will_spawn:bool = false
 var ab_to_spawn
+var spawn_point:Node3D = $SpawnPoint
 
 @export var aberration_list:AberrationList
 @export var chance:Array = [1,3]
@@ -12,6 +13,10 @@ func _ready():
 	will_spawn = get_chance([chance[0],chance[1], 3)
 	if will_spawn:
 		ab_to_spawn = aberration_list.pick_random()
+		ab_noise = ab_to_spawn.get_noise()
+		spawn_point.add_child(ab_to_spawn.instance())
+	else:
+		self.queue_free() #remove from scene if not being used
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +25,14 @@ func _process(delta):
 
 func get_chance(chance_rng:Array, chance_num:Int):
 	return RandomNumberGenerator.new().randf_range(chance_rng[0], change_rng[1]) == chance_num
+
+#TODO: CONNECT SIGNAL
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		print_debug("entered")
+		ab_to_spawn.fade_up_noise()
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+		print_debug("exited")
+		ab_to_spawn.fade_up_noise()
