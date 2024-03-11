b406e9e0e8ae86745ed7b75394c2f0bce715309f
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2024-03-11 09:53:24 -0700

    Update player_globals.gd

--- globals/player_globals.gd
+++ globals/player_globals.gd
@@ -7,14 +7,23 @@ var cash:int
 var aberritions_in_scene:int
 
 
-# dictates how long a player must 
+# dictates how long a player can be in the danger zone 
 @onready var player_death_timer = Timer.new()
+signal player_death_timer_timeout
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_death_timer()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func set_death_timer():
+	player_death_timer.wait_time = 1.0
+	player_death_timer.one_shot = true
+	player_death_timer.connect("timeout", self, "_on_player_death_timer_timeout")
+
+func _on_player_death_timer_timeout():
+	# emit_signal death signal to player
